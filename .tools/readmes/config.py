ae915c243c898c49b9254189bdac908481cab854
Jennifer Cao <jencao@amazon.com>
2026-01-07 14:37:36 -0800

    adding metadata for redshift

--- .tools/readmes/config.py
+++ .tools/readmes/config.py
@@ -220,6 +220,8 @@ language = {
                 "pinpoint": "sap-abap/services/ppt",
                 "pinpoint-sms-voice": "sap-abap/services/pps",
                 "polly": "sap-abap/services/ply",
+                "redshift": "sap-abap/services/rsh",
+                # "redshift": "sap-abap/services/rsd",
                 "rekognition": "sap-abap/services/rek",
                 "route53-recovery-cluster": "sap-abap/services/r5v",
                 "sagemaker": "sap-abap/services/sgm",
